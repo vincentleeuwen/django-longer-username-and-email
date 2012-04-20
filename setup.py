af6fe53edf3f7f2b0c72abab485f865df72a25d9
Mads Sülau Jørgensen <mads@sulau.dk>
2012-04-20 12:03:10 +0200

    Added discription in setup.py

--- setup.py
+++ setup.py
@@ -12,4 +12,10 @@ setup(
     version="0.5",
     packages=find_packages(),
     zip_safe=False,
+    description=("django-longerusernameandemail provides a migration and a "
+                 "monkeypatch to make the django auth.user username and email "
+                 "fields longer, instead of the arbitrarily short 30 and 75 "
+                 "characters."),
+    license="BSD",
+    keywords="django long email username migration south",
 )
