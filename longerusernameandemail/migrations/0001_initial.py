0a3f43a191fc43775bfc78a8436be2ceccf8ee8b
dalazx <dalazx@gmail.com>
2012-05-03 13:33:18 +0300

    django.contrib.auth.forms.AuthenticationForm doesn't have an email field. KeyError was raised. Fixed. MAX_EMAIL_LENGTH was added.

--- longerusernameandemail/migrations/0001_initial.py
+++ longerusernameandemail/migrations/0001_initial.py
@@ -2,7 +2,7 @@
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
-from longerusernameandemail import MAX_USERNAME_LENGTH
+from longerusernameandemail import MAX_USERNAME_LENGTH, MAX_EMAIL_LENGTH
 
 class Migration(SchemaMigration):
 
@@ -10,7 +10,7 @@ class Migration(SchemaMigration):
         # Changing field 'User.username'
         db.alter_column('auth_user', 'username', models.CharField(max_length=MAX_USERNAME_LENGTH()))
         # Increase length of email field to match
-        db.alter_column('auth_user', 'email', models.CharField(max_length=MAX_USERNAME_LENGTH()))
+        db.alter_column('auth_user', 'email', models.CharField(max_length=MAX_EMAIL_LENGTH()))
         # Add an index to make email field unique
         db.create_index('auth_user', ['email'], unique=True)
 
